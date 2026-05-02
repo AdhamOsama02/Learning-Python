ea366395d1ac6cca0f84b30d84decfd861fb4f81
AdhamOsama02 <adhamosama1902@gmail.com>
2026-05-02 06:43:57 +0300

    2nd code

---
+++ 002_Data_Types_&_Variables.py
@@ -0,0 +1,26 @@
+print("good morning")
+print(type("good morning"))   #str => string
+print(type(1))   #int => integer
+print(type(0.5))   #float => float
+print(type([1,2,3,4,5]))   #list => list
+print(2 == 2)   #in the terminal it is True
+print(2 == 4)   #in the terminal it is False
+print(type((1,2,3,4,5)))   #tuple => tuple
+print(type({"one" : 1,"two" : 2}))    #dict => dictionary
+print(type(2 == 2))   #bool => boolean
+
+# the variables
+namevariable = "adham"
+myname = "adham"
+yourname = "osama"
+teamname = "real madrid"
+print(namevariable)
+print(myname)
+print(yourname)
+print(teamname)
+a, b, c, d = 1,2,3,4
+print(a)
+print(b)
+print(c)
+print(d)
+
