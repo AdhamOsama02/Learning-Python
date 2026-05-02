c311fc7f93237fc837f62f61d031de1466d8d048
AdhamOsama02 <adhamosama1902@gmail.com>
2026-05-02 06:44:32 +0300

    3rd code

---
+++ 003_Escape_Squences_Characters.py
@@ -0,0 +1,31 @@
+#  Escape Sequences Characters
+# \b => Back Space
+# \newline => Escape New Line + \
+# \\ => Escape Back Slash
+# \' => Escape Single Qoutes
+# \" => Escape Double Qoutes
+# \n => Line Feed
+# \r => Carriage Return 
+# \t => Horizontal Tap
+# \xhh => Character Hex Value
+
+# \b
+print("adhamm\bosama")
+# \newline
+print("hello " \
+"adham" \
+"osama")
+# \\
+print("hello\\python")
+# \'
+print('hello adham \'osama\'') 
+# \"
+print("hello adham \"osama\"") 
+# \n
+print("adham\nosama")
+# \r
+print("123456\rabcd")
+# \t
+print("adham\tosama")
+# \xhh
+print("\x41 \x64 \x68 \x61 \x6D")
