a5b0c6eba6479462b783086bba0ce8020611d58f
AdhamOsama02 <adhamosama1902@gmail.com>
2026-05-02 06:45:07 +0300

    4th code

---
+++ 004_Concatenation_&_Strings.py
@@ -0,0 +1,31 @@
+# -------------
+#1) Concatenation
+# -------------
+
+First_Name = "Adham"
+Family_Name = "Osama"
+print(First_Name + "\n" +Family_Name)
+
+Full_Name = First_Name + "\n" +Family_Name
+print(Full_Name)
+
+# ------------
+# 2) Strings
+# ------------
+String_one = 'this is single quote'
+String_two = "this is double quotes"
+
+print(String_one)
+print(String_two)
+
+String_three = 'this is single quote "test"'
+String_four = "this is double quotes 'test'"
+
+print(String_three)
+print(String_four)
+
+String_five = """Adham
+Osama 'test' "test" 
+Rashad"""
+
+print(String_five)
