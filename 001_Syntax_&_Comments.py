331deb750e479254ce104ccde98667d2f9fea63c
AdhamOsama02 <adhamosama1902@gmail.com>
2026-05-02 06:43:23 +0300

    1st code

---
+++ 001_Syntax_&_Comments.py
@@ -0,0 +1,12 @@
+print("hallo python")
+print("hallo adham")
+print("hallo gemini")
+print("Hala madrid")
+if True:
+    print("hallo python")
+    print("hallo adham")
+    print("hallo gemini")
+    print("Hala madrid") # if you used this method you will win the UCL
+    print("real madrid"); print("madrid")
+    # this code made by adham osama and gemini
+    # print("adham")
